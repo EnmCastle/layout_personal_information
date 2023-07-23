4b3d2d9fe5b77f815eb441a32d0f0788d506cb88
EnmCastle <EnmaCastle@outlook.es>
2023-07-23 20:24:52 -0300

    Js corregido

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-addEventListener(click)
+const btn = document.querySelector(".switch");
+const lgactive = document.querySelector(".Lg")
+
+btn.addEventListener("click", function() {
+  document.body.classList.toggle("Lg");
+});
