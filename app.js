235fcadbe40bb178c3d953c1da24abc282100bed
EnmCastle <EnmaCastle@outlook.es>
2023-07-25 13:23:03 -0300

    Animação de switch e outros detalhes

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const btn = document.querySelector(".switch");
-const lgactive = document.querySelector(".Lg")
+const btn = document.querySelector(".switch button");
+let isLg = false;
+const img = document.querySelector("#profile img")
 
 btn.addEventListener("click", function() {
-  document.body.classList.toggle("Lg");
+  document.body.classList.toggle("Lg")
+  gsap.to(btn, { x: isLg ? '0%' : '100%', duration: 0.3 });
+  isLg = !isLg;
+  
+  if (isLg) {
+  img.setAttribute('src', './assets/avatar-light.png');
+} else {
+  img.setAttribute('src', './assets/avatar.png');
+}
 });
+
+
+
